6df64487526996e4b991c145972bb2bf5b24d772
George Armando <georgearmando54@gmail.com>
2020-10-23 15:57:35 +0100

    Configurando deploy

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import 'express-async-errors';
+
+import routes from './routes';
+import errorHandler from './errors/handler';
+
+import './database/connection';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errorHandler);
+
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.listen(process.env.PORT || 3333);
